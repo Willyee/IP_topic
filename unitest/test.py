48ede545b04fc2753efb8eb36365c6d6d030beb4
Willyee <willy101560@gmail.com>
2024-06-23 18:51:31 +0800

    change for submit hw

--- unitest/test.py
+++ unitest/test.py
@@ -5,9 +5,12 @@ import os
 
 
 # 初始化模型
+print("初始化模型....")
 feature_extractor = FeatureExtractor()
 transformer_model = TransformerModel()
 
+
+print("設定路徑....")
 base = "/Users/zhangyuchen/Documents/ws/IP_topic"
 # base = "/home/mrl/ws/IP_topic"
 datas_dir = "unitest/unit_input"
@@ -19,35 +22,50 @@ feature_extractor_path = base+'/'+output_dir+'/'+'feature_extractor.pth'
 transformer_model_path = base+'/'+output_dir+'/'+'transformer_model.pth'
 
 
-# 加载模型参数
+# 加載模型參數
+print("加載模型參數....")
 feature_extractor.load_state_dict(torch.load(feature_extractor_path))
 transformer_model.load_state_dict(torch.load(transformer_model_path))
 
-# 将模型设置为评估模式
+# 將模型設置為評估模式
 feature_extractor.eval()
 transformer_model.eval()
 
 
+print("輸入圖片....")
 os.chdir(base+"/"+datas_dir+"/"+floder_name+"/"+"models/picture")
 
 image_dict = {i:'_r_'+str(i*15).zfill(3)+'.png' for i in range(24)} # for example 0:'_r_000.png', 5:'_r_075.png'
-image_paths = [image_dict[i]for i in list(range(3,5))]#sorted(random.choices(list(range(24)),k=24))]
+image_paths = [image_dict[i]for i in list(range(24))]#sorted(random.choices(list(range(24)),k=24))]
 
 # 提取特徵
+
+print("提取特徵....")
 image_features = [extract_features(image_path) for image_path in image_paths]
 image_features = torch.cat(image_features, dim=0)  # 合併為一個 tensor，形狀為 (M, 1280)
+print("=================Image Features =================")
+print(image_features)
+print(image_features.shape)
 
+print("初始化decoder輸入...")
 tgt = torch.ones((1, 1, 4096))
 
-# 测试过程（与训练类似，但不进行梯度计算）
+
+# 測試過程（與訓練類似，但不進行梯度計算）
+
 with torch.no_grad():
     features = feature_extractor(image_features)
     src = features.unsqueeze(1)
     output = transformer_model(src, tgt)
+    print("=================nonreshared output=================")
+    print(output)
+    print(output.shape)
     output = output.reshape(32, 32, 32)
-    # 可以进行后续的处理或评估
+    # 可以進行後續的處理或評估
 
+print("=================reshared output=================")
 print(output)
+print(output.shape)
 voxel = output.numpy()
-voxel = voxel > 0.3
+voxel = voxel > 0.5
 display_binvox(voxel=voxel)
--- unitest/transformer.py
+++ unitest/transformer.py
@@ -5,14 +5,14 @@ from torchvision import transforms
 from PIL import Image
 import torch.optim as optim
 import os
-from display_binvox import read_binvox,display_binvox
+from display_binvox import read_binvox, display_binvox
 import math
-import random
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # EfficientNet-b0 模型
 # efficientnet = models.efficientnet_b0(pretrained=True)
+print("載入EfficientNet-b0 預訓練模型參數")
 efficientnet = models.efficientnet_b0(weights=models.EfficientNet_B0_Weights.DEFAULT)
 efficientnet.classifier = nn.Identity()  # 移除最後的分類層
 
@@ -45,8 +45,8 @@ class PicturePositionalEncoding(nn.Module):
         self.dropout = nn.Dropout(p=dropout)
 
         pe = torch.zeros(max_len, d_model)
-        position = torch.arange(0, max_len).unsqueeze(1).float()*math.pi*2/max_len
-        div_term = torch.arange(0, d_model, 2).float()*math.pi*2/d_model
+        position = torch.arange(0, max_len).unsqueeze(1).float() * math.pi * 2 / max_len
+        div_term = torch.arange(0, d_model, 2).float() * math.pi * 2 / d_model
         pe[:, 0::2] = torch.sin(position * div_term)
         pe[:, 1::2] = torch.cos(position * div_term)
         pe = pe.unsqueeze(0).transpose(0, 1)
@@ -60,7 +60,7 @@ class TransformerModel(nn.Module):
     def __init__(self, input_dim=4096, num_heads=8, num_layers=6, dim_feedforward=2048):
         super(TransformerModel, self).__init__()
         self.pos_encoder = PicturePositionalEncoding(input_dim, max_len=24)
-        self.encoder_layer = nn.TransformerEncoderLayer(d_model=input_dim, nhead=num_heads, dim_feedforward=dim_feedforward,batch_first=True)
+        self.encoder_layer = nn.TransformerEncoderLayer(d_model=input_dim, nhead=num_heads, dim_feedforward=dim_feedforward, batch_first=True)
         self.transformer_encoder = nn.TransformerEncoder(self.encoder_layer, num_layers=num_layers)
         self.decoder_layer = nn.TransformerDecoderLayer(d_model=input_dim, nhead=num_heads, dim_feedforward=dim_feedforward)
         self.transformer_decoder = nn.TransformerDecoder(self.decoder_layer, num_layers=num_layers)
@@ -78,12 +78,15 @@ class TransformerModel(nn.Module):
 
 
 if __name__ == "__main__":
+    # 初始化模型
+    print("初始化模型....")
     feature_extractor = FeatureExtractor()
     transformer_model = TransformerModel()
 
 
     # 假設我們有 M 張照片，ground truth 為 (32, 32, 32) 的 Voxel 模型
     # 輸入照片的路徑
+    print("設定路徑....")
     base = "/Users/zhangyuchen/Documents/ws/IP_topic"
     # base = "/home/mrl/ws/IP_topic"
     datas_dir = "unitest/unit_input"
@@ -91,62 +94,76 @@ if __name__ == "__main__":
 
     output_dir = "unitest/unit_output"
 
-    feature_extractor_path = base+'/'+output_dir+'/'+'feature_extractor.pth'
-    transformer_model_path = base+'/'+output_dir+'/'+'transformer_model.pth'
+    feature_extractor_path = base + '/' + output_dir + '/' + 'feature_extractor.pth'
+    transformer_model_path = base + '/' + output_dir + '/' + 'transformer_model.pth'
 
-
-    os.chdir(base+"/"+datas_dir+"/"+floder_name+"/"+"models/picture")
-
-    image_dict = {i:'_r_'+str(i*15).zfill(3)+'.png' for i in range(24)} # for example 0:'_r_000.png', 5:'_r_075.png'
-    image_paths = [image_dict[i]for i in list(range(24))]#sorted(random.choices(list(range(24)),k=24))]
+    os.chdir(base + "/" + datas_dir + "/" + floder_name + "/" + "models/picture")
+    
+    print("輸入圖片....")
+    image_dict = {i: '_r_' + str(i * 15).zfill(3) + '.png' for i in range(24)}  # 例如 0:'_r_000.png', 5:'_r_075.png'
+    image_paths = [image_dict[i] for i in list(range(24))]  # sorted(random.choices(list(range(24)),k=24))]
 
     # 提取特徵
+    print("將圖片轉換成CNN模型預設輸入....")
     image_features = [extract_features(image_path) for image_path in image_paths]
     image_features = torch.cat(image_features, dim=0)  # 合併為一個 tensor，形狀為 (M, 1280)
 
-    # ground truth Voxel 模型
-    ground_truth_voxel = torch.from_numpy(read_binvox(base,datas_dir,floder_name))
-
+   
     # 準備 transformer 的輸入
+    print("初始化decoder輸入...")
     tgt = torch.ones((1, 1, 4096))
+
+    # ground truth Voxel 模型
+    print("讀取decoder輸出groud ture...")
+    ground_truth_voxel = torch.from_numpy(read_binvox(base, datas_dir, floder_name))
 
 
     # 訓練過程
+    print("設定優化器與損失函數...")
     optimizer = optim.Adam(list(feature_extractor.parameters()) + 
                         list(transformer_model.parameters()), lr=1e-4)
     schedule = optim.lr_scheduler.StepLR(optimizer, step_size=20, gamma=0.1)
     criterion = nn.BCELoss()
 
-    num_epochs = 10 # 訓練的總迭代次數
+    print("開始訓練...")
+    num_epochs = 10  # 訓練的總迭代次數
     for epoch in range(num_epochs):
+        print("========Epoch:========", epoch+1)
         feature_extractor.train()
         transformer_model.train()
         optimizer.zero_grad()
-        features = feature_extractor(image_features)  # 確保在每個epoch中重新計算特徵
-        src = features.unsqueeze(1) # 形狀為 (M, 1, 4096)
+        print("提取圖片特徵....")
+        features = feature_extractor(image_features)  # 確保在每個 epoch 中重新計算特徵
+        src = features.unsqueeze(1)  # 形狀為 (M, 1, 4096)
+        print("訓練transformer....")
         output = transformer_model(src, tgt)  # 形狀為 (9, 1, 4096)
 
-        # 將輸出重塑為 (32, 32, 4) 並比較 ground truth
+        # 將輸出重塑為 (32, 32, 32) 並比較 ground truth
         output = output.reshape(32, 32, 32)
+        print("計算損失....")
         loss = criterion(output, ground_truth_voxel)
+        print("反向傳播....")
         loss.backward(retain_graph=True)  # 確保在需要時保留計算圖
+        print("優化....")
         optimizer.step()
         schedule.step()  # 調整學習率
-        
         
         print(f"Epoch {epoch+1}, Loss: {loss.item()}, lr: {optimizer.param_groups[0]['lr']}")
 
     # 測試過程（與訓練類似，但不進行梯度計算）
     transformer_model.eval()
     with torch.no_grad():
+        print("測試transformer....")
         output = transformer_model(src, tgt)
         output = output.reshape(32, 32, 32)
         # 可以進行後續的處理或評估
         print(output)
         voxel = output.numpy()
-        voxel = voxel > 0.3
+        voxel = voxel > 0.5
+        print("展示....(記得關閉視窗後才能繼續執行下一步，下一步是保存模型參數)")
         display_binvox(voxel=voxel)
 
-    # 保存模型参数
+    # 保存模型參數
+    print("保存模型參數....")
     torch.save(feature_extractor.state_dict(), feature_extractor_path)
     torch.save(transformer_model.state_dict(), transformer_model_path)
